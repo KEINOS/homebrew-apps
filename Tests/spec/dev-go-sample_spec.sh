c163f05099a894df87bf29f5e01d9add55474553
KEINOS <github@keinos.com>
2022-01-11 14:55:43 +0900

    feat: monthly test for dev-go repo's release

---
+++ Tests/spec/dev-go-sample_spec.sh
@@ -0,0 +1,18 @@
+#shellcheck shell=sh
+
+Describe 'dev-go-sample'
+  # Download Test
+  It 'should success on install via brew'
+    When call brew install KEINOS/apps/dev-go-sample
+    The status should be success
+    The stderr should include "Tapping keinos/apps"
+    The output should include "🍺  /home/linuxbrew/.linuxbrew/Cellar/dev-go-sample"
+  End
+
+  # Smoke Test
+  It 'should success and contain "pallet" in the output on version print'
+    When run dev-go-sample
+    The status should be success
+    The output should match pattern "Hello, Gopher!*"
+  End
+End
