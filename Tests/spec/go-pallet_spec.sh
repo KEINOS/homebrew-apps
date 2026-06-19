54453253c48ae01d2add4bd3f76b2da2d69b3ffc
KEINOS <github@keinos.com>
2026-06-19 16:58:50 +0900

    fix: migrate go-pallet tests from formula to cask

--- Tests/spec/go-pallet_spec.sh
+++ Tests/spec/go-pallet_spec.sh
@@ -3,11 +3,11 @@
 Describe 'go-pallet'
   # Download Test
   It 'should success on install via brew'
-    When call brew install --verbose KEINOS/apps/go-pallet
+    When call brew install --cask --verbose KEINOS/apps/go-pallet
     The status should be success
     The stderr should include "Tapping keinos/apps"
     The output should include "🍺"
-    The output should include "/Cellar/go-pallet"
+    The output should include "/Caskroom/go-pallet"
   End
 
   # Smoke Test
